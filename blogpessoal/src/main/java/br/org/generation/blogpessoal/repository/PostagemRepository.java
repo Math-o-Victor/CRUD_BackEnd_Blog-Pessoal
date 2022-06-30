36846974be2a39a30d5a3e6681eac4ffbf691c58
Math-o-Victor <106315792+Math-o-Victor@users.noreply.github.com>
2022-06-30 13:32:47 -0300

    Atualizando o 'Controller' e o 'Repository' para que possam adicionar, deletar, atualizar e retornar dados.

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,7 +1,5 @@
 package br.org.generation.blogpessoal.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +8,8 @@ import br.org.generation.blogpessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
-		public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+		Object findAllByTituloContainingIgnoreCase(String titulo);
+
+		
 
 }
