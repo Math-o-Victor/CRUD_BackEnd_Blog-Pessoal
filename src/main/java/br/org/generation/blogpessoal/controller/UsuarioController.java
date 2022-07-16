1f5336b10637b4843f1aec79fad5099ab4d6734d
Math-o-Victor <106315792+Math-o-Victor@users.noreply.github.com>
2022-07-16 11:11:25 -0300

    Update deletar usuario

--- src/main/java/br/org/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/br/org/generation/blogpessoal/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,6 +69,12 @@ import br.org.generation.blogpessoal.service.UsuarioService;
 					.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
 			
 		}
+		
+		@DeleteMapping("/{id}")
+		public void delete(@PathVariable Long id) {
+			usuarioRepository.deleteById(id);
+		}
+		
 		
 					
 		
